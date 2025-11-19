7ba2a8ace1ecc934f61543100073441e957a59ff
SAWYER SAWYER <mehaleka240399@yandex.ru>
2025-11-20 00:01:22 +0300

    feat: настроен стиль эмоций

--- src/components/MoodButtons/MoodButtons.tsx
+++ src/components/MoodButtons/MoodButtons.tsx
@@ -1,28 +1,36 @@
 import React from "react";
 import type { MoodButtonsProps } from "../../types";
 import styles from "./MoodButtons.module.css";
+//эмодзи настроений
+import Good from '../../assets/images/moods/good_mood.svg';
+import Neutral from '../../assets/images/moods/neutral_mood.svg';
+import Sad from '../../assets/images/moods/sad_ mood.svg';
 
 const MoodButtons: React.FC<MoodButtonsProps> = ({
   selectedMood,
   onSelect,
 }) => {
   //массив эмодзи
-  const moods = ["😄", "😐", "😣"];
+  const moods = [
+    { id: 'good', img: Good },
+    { id: 'neutral', img: Neutral },
+    { id: 'sad', img: Sad},
+  ];
 
   return (
     <div className={styles.container}>
-      {moods.map((emoji) => (
+      {moods.map((mood) => (
         <button
-          key={emoji}
+          key={mood.id}
           type="button"
           className={
-            selectedMood === emoji
+            selectedMood === mood.id
               ? `${styles.button} ${styles.buttonSelected}`
               : styles.button
           }
-          onClick={() => onSelect(emoji)}
+          onClick={() => onSelect(mood.id)}
         >
-          {emoji}
+          <img src={mood.img} alt={mood.img} className={styles.moodImg} />
         </button>
       ))}
     </div>
